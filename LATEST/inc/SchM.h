60dcf97b4cfeca870e84c204b6413bcca202a61a
root <nagaraja.hm.0001@gmail.com>
2021-12-14 14:22:22 +0100

    Update: submodule SchM

---
+++ LATEST/inc/SchM.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class class_SchM{
+   public:
+      void Start          (void);
+      void InitFunction   (void);
+      void DeInitFunction (void);
+      void StartTiming    (void);
+};
+
+extern class_SchM SchM;
+
---
+++ LATEST/src/SchM.cpp
@@ -0,0 +1,16 @@
+#include "SchM.h"
+
+class_SchM SchM;
+
+void class_SchM::Start(void){
+}
+
+void class_SchM::InitFunction(void){
+}
+
+void class_SchM::DeInitFunction(void){
+}
+
+void class_SchM::StartTiming(void){
+}
+
