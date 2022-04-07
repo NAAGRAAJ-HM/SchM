652d9252e343a2baae68907d27eccb3d84186a89
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:51:30 +0200

    Update: AUTOSAR basic software service module SchM

--- LATEST/SchM.cpp
+++ LATEST/SchM.cpp
@@ -101,8 +101,14 @@ FUNC(void, SCHM_CODE) module_SchM::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_SchM as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == SchM_InitCheck)
