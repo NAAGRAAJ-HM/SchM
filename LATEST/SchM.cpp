79d5d4e3e3e9bd1dfe37fc8e0fb302b9b3b7e8d6
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:07:38 +0200

    Update: AUTOSAR service submodule - SchM

--- LATEST/SchM.cpp
+++ LATEST/SchM.cpp
@@ -76,6 +76,10 @@ CONSTP2VAR(infSchM_EcuM,  SCHM_VAR, SCHM_CONST) gptrinfSchM_EcuM       = &SchM;
 /******************************************************************************/
 VAR(module_SchM, SCHM_VAR) SchM(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          SCHM_AR_RELEASE_VERSION_MAJOR
       ,  SCHM_AR_RELEASE_VERSION_MINOR
       ,  0x00
