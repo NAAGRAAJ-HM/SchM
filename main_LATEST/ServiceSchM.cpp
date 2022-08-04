6f00a94a88f68ad2bd948d553bb18b44fb5f6213
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:54:15 +0200

    Update: AUTOSAR basic software service module for schedule manager

---
+++ main_LATEST/ServiceSchM.cpp
@@ -0,0 +1,185 @@
+/******************************************************************************/
+/* File   : ServiceSchM.cpp                                                          */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "ServiceSchM.hpp"
+#include "infServiceSchM_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SCHM_AR_RELEASE_VERSION_MAJOR                                          4
+#define SCHM_AR_RELEASE_VERSION_MINOR                                          3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(SCHM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible SCHM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(SCHM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible SCHM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_ServiceSchM, SCHM_VAR) ServiceSchM;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, SCHM_CODE) module_ServiceSchM::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, SCHM_CONST,       SCHM_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   SCHM_CONFIG_DATA, SCHM_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == ServiceSchM_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstServiceSchM_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == ServiceSchM_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == ServiceSchM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ServiceSchM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SCHM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::DeInitFunction(
+   void
+){
+#if(STD_ON == ServiceSchM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceSchM_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == ServiceSchM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SCHM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::MainFunction(
+   void
+){
+#if(STD_ON == ServiceSchM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceSchM_InitCheck)
+   }
+   else{
+#if(STD_ON == ServiceSchM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SCHM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::Start(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::StartTiming(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::ActMainFunction(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::McalCancelMainFunction(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::Enter(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::Exit(
+   void
+){
+}
+
+FUNC(void, SCHM_CODE) module_ServiceSchM::ServiceSchM_1ms(void){
+   for(
+      uint8 lu8Index = 0;
+            lu8Index < lptrConst->u8SizeServiceSchMClients;
+            lu8Index ++
+   ){
+      lptrConst->aptrServiceSchMClients_1ms[lu8Index]->MainFunction();
+   }
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
