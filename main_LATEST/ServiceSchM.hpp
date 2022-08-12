c4e745dc682e29d143b43c75984820cac0897044
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-12 16:15:53 +0200

    Initial commit

--- main_LATEST/ServiceSchM.hpp
+++ main_LATEST/ServiceSchM.hpp
@@ -24,7 +24,7 @@
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
 class module_ServiceSchM:
-      INTERFACES_EXMCALPORTED_SERVICESCHM
+      INTERFACES_EXPORTED_SERVICESCHM
    ,  public abstract_module
    ,  public class_ServiceSchM_Functionality
 {
--- main_LATEST/infServiceSchM_Exp.hpp
+++ main_LATEST/infServiceSchM_Exp.hpp
@@ -14,7 +14,7 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define INTERFACES_EXMCALPORTED_SERVICESCHM                   public infServiceSchM_ServiceEcuM         \
+#define INTERFACES_EXPORTED_SERVICESCHM                   public infServiceSchM_ServiceEcuM         \
                                                 ,  public infServiceSchM_ServiceSwcOs \
 
 /******************************************************************************/
