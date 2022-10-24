a00001c1e3ab1898112831d59afe903a08fd0aa8
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:03:00 +0200

    Update: AUTOSAR basic software service module for schedule manager

---
+++ main_R2022/ServiceSchM.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceSchM.hpp                                                          */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstServiceSchM.hpp"
+#include "CfgServiceSchM.hpp"
+#include "ServiceSchM_core.hpp"
+#include "infServiceSchM_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_ServiceSchM:
+      INTERFACES_EXPORTED_SERVICESCHM
+   ,  public abstract_module
+   ,  public class_ServiceSchM_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstServiceSchM_Type* lptrConst = (ConstServiceSchM_Type*)NULL_PTR;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, SERVICESCHM_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, SERVICESCHM_CONST,       SERVICESCHM_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICESCHM_CONFIG_DATA, SERVICESCHM_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, SERVICESCHM_CODE) DeInitFunction (void);
+      FUNC(void, SERVICESCHM_CODE) MainFunction   (void);
+      SERVICESCHM_CORE_FUNCTIONALITIES
+
+      FUNC(void, SERVICESCHM_CODE) Start          (void);
+      FUNC(void, SERVICESCHM_CODE) StartTiming    (void);
+      FUNC(void, SERVICESCHM_CODE) ServiceSchM_1ms       (void);
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_ServiceSchM, SERVICESCHM_VAR) ServiceSchM;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
